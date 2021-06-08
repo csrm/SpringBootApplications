f6688d9f0632e4b88b7a0327b7921ec778ed2ba3
Chivukula Sree Mrudula <chivukula.mrudula@gmail.com>
2021-06-08 14:37:08 +0530

    Handled POST call

--- rest-service/src/main/java/com/example/restservice/GreetingController.java
+++ rest-service/src/main/java/com/example/restservice/GreetingController.java
@@ -2,7 +2,12 @@ package com.example.restservice;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +22,16 @@ public class GreetingController {
                       System.out.println(name);
                       return new Greeting(counter.incrementAndGet(),String.format( 
                           template, name ));
+    }
+
+    @GetMapping("/greet/{id}")
+    public Greeting greetingId(@PathVariable String id) {
+        System.out.println("In Path Variable method");
+        return new Greeting(counter.incrementAndGet(), id);
+    }
+
+    @PostMapping("/greeting")
+    public @ResponseBody ResponseEntity<String> post() {
+        return new ResponseEntity<String>("Data Posted",HttpStatus.OK);
     }
 }

bbc629450b4fa6169d01011c4deaa139d85331b4
Chivukula Sree Mrudula <chivukula.mrudula@gmail.com>
2021-06-27 19:12:35 +0530

    Modified rest-service to respond back with XML

--- rest-service/src/main/java/com/example/restservice/Greeting.java
+++ rest-service/src/main/java/com/example/restservice/Greeting.java
@@ -1,8 +1,20 @@
 package com.example.restservice;
-
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.PROPERTY)
 public class Greeting {
+    @XmlElement(name = "id")
     private final long id;
+    @XmlElement(name = "content")
     private final String content;
+
+    public Greeting( ) {
+        this.id = 1;
+        this.content = "Intial Content";
+    }
 
     public Greeting( long id, String content) {
         this.id = id;
--- rest-service/src/main/java/com/example/restservice/GreetingController.java
+++ rest-service/src/main/java/com/example/restservice/GreetingController.java
@@ -4,11 +4,14 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,7 +19,8 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @GetMapping("/greeting")
+    @RequestMapping(value="/greeting", method=RequestMethod.GET, produces = MediaType.APPLICATION_XML_VALUE, consumes = MediaType.ALL_VALUE )
+    @ResponseBody
     public Greeting greeting(@RequestParam( value = "name", defaultValue = 
                   "World") String name) {
                       System.out.println(name);
